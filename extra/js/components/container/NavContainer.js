786c3901f956005f6daa932e614bb9b461ca11ed
William Strong <william.strong@me.com>
2018-07-11 21:10:54 -0700

    Add extra files which may be useful later

---
+++ extra/js/components/container/NavContainer.js
@@ -0,0 +1,10 @@
+<div class="title_nav">
+    <p id="name">Alexandra Forest</p>
+    <div>
+        <span id="nav_left_side"><a href="about_me">About me</a></span>
+        <!--<img src="img/line" />-->
+        <span id="nav_center"><a href="art">Art</a></span>
+        <!--<img src="img/line" />-->
+        <span id="nav_right_side"><a href="contact">Contact</a></span>
+    </div>
+</div>
