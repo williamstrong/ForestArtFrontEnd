bbca30893b37bd250e1a49a66665c9ba66294fc4
William Strong <william.strong@me.com>
2018-07-23 22:16:30 -0700

    Start preview

--- src/components/container/PreviewContainer.jsx
+++ src/components/container/PreviewContainer.jsx
@@ -5,30 +5,63 @@ import Title from '../presentational/Title';
 
 import AbstractHeader from '../../../img/header/abstract/home_page.png';
 
+const styles = {
+  previewLayout: {
+    display: 'flex',
+    flexFlow: 'row wrap ',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    alignContent: 'center',
+  },
+};
+
+const previewImages = [
+  {
+    id: 'stringId',
+    name: 'imageName',
+    src: AbstractHeader,
+    alt: 'imageAltText',
+  },
+  {
+    id: 'stringId',
+    name: 'imageName',
+    src: AbstractHeader,
+    alt: 'imageAltText',
+  },
+];
+
 export default class PreviewContainer extends Component {
   constructor() {
     super();
     this.state = {
-
+      // Images can be enlarged. This indicates which ImageBox is enlarged.
+      enlarged: 0,
+      previewImages,
     };
   }
 
+  handleClick(id) {
+      debugger;
+  }
+
   render() {
+    const imageBoxes = previewImages.map(
+      previewImage => (
+          <button onClick={() => this.handleClick(previewImage.id)}>
+          <ImageBox
+            key={previewImage.id}
+            name={previewImage.name}
+            src={previewImage.src}
+            alt={previewImage.alt}
+          />
+        </button>
+      ),
+    );
     return (
       <div>
         <Title name="My latest work" />
-        <div id="preview">
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
-
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
-
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
-          <ImageBox name="testImage" src={AbstractHeader} />
+        <div style={styles.previewLayout}>
+          { imageBoxes }
         </div>
       </div>
     );
