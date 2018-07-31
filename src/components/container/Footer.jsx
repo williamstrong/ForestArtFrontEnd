d6a6343aa4a4a04b529baef7a380e7cc35caabf9
William Strong <william.strong@me.com>
2018-07-30 19:59:24 -0700

    Add footer

---
+++ src/components/container/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { css } from 'emotion';
+import InstagramLogo from '../../../img/brand_assets/instagram_logo.png';
+
+const altText = "Alexandra Forest's instagram page.";
+const igLink = 'http://www.instagram.com/aforestart';
+
+const styles = {
+  footer: css({
+    width: '100%',
+    background: 'grey',
+  }),
+  flex: css({
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignContent: 'center',
+  }),
+  text: css({
+    margin: '15',
+    color: 'white',
+    font: '2em Europa-Light',
+  }),
+  link: css({
+    margin: '15',
+    flexBasis: 'auto',
+  }),
+  instagram: css({
+    height: '30px',
+    width: '30px',
+  }),
+};
+
+const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles.flex}>
+      <p className={styles.text}>
+      A Forest Art
+      </p>
+      <a href={igLink} className={styles.link}>
+        <img src={InstagramLogo} alt={altText} className={styles.instagram} />
+      </a>
+    </div>
+  </div>
+);
+
+export default Footer;
