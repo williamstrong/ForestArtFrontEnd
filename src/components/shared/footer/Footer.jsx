fb6e4e2904add022bea50574095c020aa6353e8f
William Strong <william.strong@me.com>
2018-08-08 08:24:22 -0700

    Make footer go to bottom of document

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -9,8 +9,6 @@ const igLink = 'http://www.instagram.com/aforestart';
 
 const styles = {
   footer: css({
-    position: 'absolute',
-    bottom: '0',
     marginTop: '10%',
     width: '100%',
     background: 'grey',
