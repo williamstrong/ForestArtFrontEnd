b7c3042faba2413e1110e876eb6bb2ed258740e6
William Strong <william.strong@me.com>
2018-09-19 17:04:38 -0700

    Add favicons and fonts to html template

--- src/server/index.js
+++ src/server/index.js
@@ -22,7 +22,7 @@ app.get('*', (req, res, next) => {
       <head>
         <meta charset="utf-8">
 
-        <link rel="dns-prefetch" href="//s3.amazonaws.com">
+        <link rel="dns-prefetch" href="https://s3-us-west-1.amazonaws.com">
 
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title></title>
@@ -31,10 +31,34 @@ app.get('*', (req, res, next) => {
 
         <link rel="manifest" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/site.webmanifest">
         <link rel="apple-touch-icon" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/icon.png">
-        <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/css/normalize.css">
         <link rel="stylesheet" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/css/main.css">
+
+        <link rel="stylesheet" href="https://use.typekit.net/nhp1iyx.css">
+
+
+        <link type="text/plain" rel="author" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/humans.txt" />
+        <link rel="shortcut icon" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon.ico">
+        <link rel="icon" sizes="16x16 32x32 64x64" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon.ico">
+        <link rel="icon" type="image/png" sizes="196x196" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-192.png">
+        <link rel="icon" type="image/png" sizes="160x160" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-160.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-96.png">
+        <link rel="icon" type="image/png" sizes="64x64" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-64.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-16.png">
+        <link rel="apple-touch-icon" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-180.png">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-TileImage" content="https://s3-us-west-1.amazonaws.com/aforest-web/assets/favicon-144.png">
+        <meta name="msapplication-config" content="https://s3-us-west-1.amazonaws.com/aforest-web/assets/browserconfig.xml">
       </head>
 
       <body>
